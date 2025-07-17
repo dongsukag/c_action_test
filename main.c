8cd327c04e9d835142e42422206940cfce3c9600
dongsukag <abcdzip777@gmail.com>
2025-07-17 10:04:16 +0900

    야호

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 int main(void)
 {
     int i;
-    for(i = 1; i < 9; i++){
-    printf("%d.github action c언어 test\n",i);
+    for(i = 1; i < 7; i++){
+    printf("%d.github action c언어 test 야호\n",i);
     }
     printf("hi\n");
     return 0;
